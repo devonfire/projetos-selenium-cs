fb4eaf25e58d31669bc477afe5d11ec9836c83a3
Gafanhoto-DevAndTester <jos.rbsantos@gmail.com>
2021-04-22 20:51:30 -0300

    Incluido o rpa-consultor-de-cep

---
+++ rpa-consultor-de-cep/RPA_ConsultorDeCEP/RPA_ConsultorDeCEP/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RPA_ConsultorDeCEP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCEP = new System.Windows.Forms.TextBox();
+            this.btnConsulta = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bell MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(273, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Informe o CEP para a consulta";
+            // 
+            // txtCEP
+            // 
+            this.txtCEP.Location = new System.Drawing.Point(90, 37);
+            this.txtCEP.Name = "txtCEP";
+            this.txtCEP.Size = new System.Drawing.Size(205, 20);
+            this.txtCEP.TabIndex = 1;
+            // 
+            // btnConsulta
+            // 
+            this.btnConsulta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnConsulta.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnConsulta.Font = new System.Drawing.Font("Bell MT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConsulta.Location = new System.Drawing.Point(90, 273);
+            this.btnConsulta.Name = "btnConsulta";
+            this.btnConsulta.Size = new System.Drawing.Size(205, 39);
+            this.btnConsulta.TabIndex = 2;
+            this.btnConsulta.Text = "Consultar CEP";
+            this.btnConsulta.UseVisualStyleBackColor = true;
+            this.btnConsulta.Click += new System.EventHandler(this.btnConsulta_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(14, 106);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 13);
+            this.lblResultado.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 330);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConsulta);
+            this.Controls.Add(this.txtCEP);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Consultor de CEP";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCEP;
+        private System.Windows.Forms.Button btnConsulta;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ rpa-consultor-de-cep/RPA_ConsultorDeCEP/RPA_ConsultorDeCEP/Form1.cs
@@ -0,0 +1,78 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RPA_ConsultorDeCEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsulta_Click(object sender, EventArgs e)
+        {
+            string url = "https://buscacepinter.correios.com.br/app/endereco/index.php";
+            string cep = txtCEP.Text;
+            string resultado = "";
+            Regex padrao;
+
+            #region Setup
+            string path = System.AppDomain.CurrentDomain.BaseDirectory.ToString();
+      
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("user-data-dir=" + path + "ChromeDriver\\Cache");
+
+            ChromeDriver driver = new ChromeDriver(path, options);
+            driver.Navigate().GoToUrl(url);
+            #endregion
+
+            #region Elements
+            IWebElement campoDeBuscaDeCEP = driver.FindElement(By.XPath("//input[@id='endereco']"));
+            IWebElement botaoBuscar = driver.FindElement(By.XPath("//button[@id='btn_pesquisar']"));
+            List<IWebElement> tabelaResultante = driver.FindElements(By.XPath("//table[@id='resultado-DNEC']")).ToList();
+            #endregion
+
+            #region Actions
+            InserirCEP();
+            ClicarNoBotaoBuscar();
+            EsperarResultadoDaBusca();
+            MostrarResultadosObtidos();
+            #endregion
+
+            void InserirCEP()
+            {
+                campoDeBuscaDeCEP.Click();
+                campoDeBuscaDeCEP.SendKeys(cep.Replace("-", ""));
+            }
+            void ClicarNoBotaoBuscar() => botaoBuscar.Click();
+            void EsperarResultadoDaBusca()
+            {
+                IWebElement telaResultanteDaConsulta = (new WebDriverWait(driver, TimeSpan.FromSeconds(50)).Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@id='resultado']"))));       
+            }
+            void MostrarResultadosObtidos()
+            {
+
+                padrao = new Regex("(Logradouro/Nome\\.?|Bairro/Distrito\\.?|Localidade/UF\\.?|CEP\\.?)");
+                
+                foreach (IWebElement item in tabelaResultante)
+                {
+
+                    resultado = resultado + "Resultado da Busca: \n\n" + padrao.Replace(item.Text,"");//
+                }
+                lblResultado.Text=resultado;
+                
+            }
+
+         //driver.Quit();
+            
+
+        }
+    }
+}
